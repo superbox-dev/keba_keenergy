a10f76d9cd78b233f97a2e3d9a4780b7a263bdfe
Michael Hacker <mh@superbox.one>
2026-08-11 07:01:04 +0200

    Add missing type hints

--- custom_components/keba_keenergy/const.py
+++ custom_components/keba_keenergy/const.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import Final
 
-ATTR_CONFIG_ENTRY: Final = "config_entry"
+ATTR_CONFIG_ENTRY: Final[str] = "config_entry"
 ATTR_OFFSET: Final[str] = "offset"
 CONF_EXTERNAL_HEAT_SOURCE_TICK: Final[str] = "scan_interval_tick_external_heat_source"
 CONF_BUFFER_TANK_TICK: Final[str] = "scan_interval_tick_buffer_tank"
@@ -16,16 +16,16 @@ CONF_SOLAR_CIRCUIT_TICK: Final[str] = "scan_interval_tick_solar_circuit"
 CONF_SWITCH_VALVE_TICK: Final[str] = "scan_interval_tick_switch_valve"
 CONF_SYSTEM_TICK: Final[str] = "scan_interval_tick_system"
 CONFIG_ENTRY_VERSION: Final[int] = 1
-DEFAULT_SCAN_INTERVAL = 20
+DEFAULT_SCAN_INTERVAL: Final[int] = 20
 DEFAULT_SSL: Final[bool] = False
 DOMAIN: Final[str] = "keba_keenergy"
 FLASH_WRITE_LIMIT_PER_WEEK: Final[int] = 30
 FLASH_WRITE_DELAY: Final[float] = 1
-MANUFACTURER: Final = "KEBA"
-MANUFACTURER_MTEC: Final = "M-TEC"
-MANUFACTURER_INO: Final = "ino"
-MIN_SCAN_INTERVAL = 20
-NAME: Final = "KeEnergy"
+MANUFACTURER: Final[str] = "KEBA"
+MANUFACTURER_MTEC: Final[str] = "M-TEC"
+MANUFACTURER_INO: Final[str] = "ino"
+MIN_SCAN_INTERVAL: Final[int] = 20
+NAME: Final[str] = "KeEnergy"
 REQUEST_REFRESH_COOLDOWN: Final[float] = 0.5
 SCAN_INTERVAL: Final[int] = 20
 
